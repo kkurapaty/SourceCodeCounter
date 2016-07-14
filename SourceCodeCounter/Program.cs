2972081452e18d843ef07c7a0557d7dfd41c4dbf
kkurapaty <kkurapaty@outlook.com>
2016-07-14 17:19:58 +0100

    Add files via upload

---
+++ SourceCodeCounter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using SourceCodeCounter.GUI;
+
+namespace SourceCodeCounter
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SplashScreen());
+        }
+    }
+}
