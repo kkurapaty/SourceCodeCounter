80d534404c38a875097f6308b9e31744d1f7766f
kkurapaty <kkurapaty@outlook.com>
2016-07-15 10:23:04 +0100

    Add files via upload

---
+++ SourceCodeCounter/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Source Code Scanner")]
+[assembly: AssemblyDescription("Source Code lines scanner developed by Kiran Kurapaty.\n Please mail me your comments or suggestions.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Morgan Stanley")]
+[assembly: AssemblyProduct("Source Code Scanner")]
+[assembly: AssemblyCopyright("Copyright © Morgan Stanley 2015. All rights reserved")]
+[assembly: AssemblyTrademark("Kiran Kurapaty")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("354337a7-061d-4a5f-adbc-8c029eb78204")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
